72173541d0e5f71675dc0bd6522eb78902bb6236
Ron Bruun <ronald.bruun@libertymutual.com>
2017-06-06 08:48:19 -0400

    cleaned up code

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -6,23 +6,24 @@
         getRandomQuestion();
 
         function getRandomQuestion() {
-            $.get("http://jservice.io/api/random", function(data) {
+            $.get("http://jservice.io/api/random", function (data) {
                 $("#questionID").text(data[0].id);
                 $("#category").text(data[0].category.title);
                 $("#question").text(data[0].question);
-                
+
                 answer = data[0].answer;
                 value = data[0].value
             });
         }
 
         $("#submit").click(function () {
-            console.log($("#answer").val() + " : " + answer);
-            if ($("#answer").val().toLowerCase() == answer.toLowercase()) {
+            let enteredAnswer = $("#answer");
+            console.log(enteredAnswer.val() + " : " + answer);
+            if (enteredAnswer.val().toLowerCase() == answer.toLowerCase()) {
                 console.log("correct");
                 $("#score").text(parseInt($("#score").text()) + value);
             }
-            $("#answer").val("");
+            enteredAnswer.val("");
             getRandomQuestion();
         })
     });
