d6f1b95ddccced5961caf216197059349caae1f7
Ron Bruun <ronald.bruun@libertymutual.com>
2017-06-05 21:42:25 -0400

    commented and cleaned up javascript

--- assets/javascript/mainhard.js
+++ assets/javascript/mainhard.js
@@ -3,9 +3,6 @@
     $(function () {
         let answer;
         let value;
-        let questionid = $("#questionID");
-        let category = $("#category");
-        let question = $("#question");
         let score = $("#score");
         let submitButton = $("#submit");
         let answerInput = $("#answer");
@@ -15,49 +12,46 @@
             return Math.floor(Math.random() * (max - min)) + min;
         }
 
-        $("button[catid]").each(function () {
-            let button = $(this);
-            $.get("http://jservice.io/api/categories?offset=" + getRandom(1, 17000), function (data) {
-                button.text(data[0].title);
-                button.attr("catid", data[0].id);
-                button.attr("title", data[0].title);
-            });
-
-        })
+        // select each of the category divs, loop over each of them and call api to get a category for each
         $(".category").each(function () {
             let thisdiv = $(this);
             $.get("http://jservice.io/api/categories?offset=" + getRandom(1, 17000), function (data) {
                 thisdiv.text(data[0].title);
+                //set the cat id on the parent div so later it is easier to get this no matter which question div is selected
                 thisdiv.parent().attr("catid", data[0].id);
-                //button.attr("title", data[0].title);
             });
 
         })
+        
+        // this function is executed whenever a question category/value is clicked
         $(".question").click(function () {
             let clickeddiv = $(this);
+            // first check if this question has already been clicked
             if (clickeddiv.attr("clicked") == "false") {
+                // get the category id from the parent div
                 let catid = clickeddiv.parent().attr("catid");
                 let qvalue = clickeddiv.attr("value");
                 let query = "?category=" + catid + "&value=" + qvalue;
                 result.text("");
                 console.log(query);
+                // call the api for the specific category and question value that was clicked
                 $.get("http://jservice.io/api/clues" + query, function (data) {
-                    questionid.text(data[0].id);
-                    category.text(data[0].category.title);
-                    question.text(data[0].question);
                     answer = data[0].answer;
                     value = data[0].value;
+                    //put the question text in the div that was clicked
                     clickeddiv.text(data[0].question);
+                    // add the class to display the question in the smaller font
                     clickeddiv.addClass("questiontext");
+                    // set the attribute so this question can't be clicked again
                     clickeddiv.attr("clicked", "true");
                 });
             }
         });
 
+        // this function is called whenever the submit button is clicked
         submitButton.click(function () {
             console.log(answerInput.val().toLowerCase() + " : " + answer.toLowerCase() + " : " + value);
             if (answerInput.val().toLowerCase() == answer.toLowerCase()) {
-                console.log("correct");
                 score.text(parseInt(score.text()) + value);
                 result.text("That is correct");
             } else {
